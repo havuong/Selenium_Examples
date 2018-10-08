f1ecdfeef5c6c4fadbb9212fb513c8670083a3dc
havuong <havuong138@gmail.com>
2018-10-08 18:18:05 +0700

    [TestNG] Annotation

---
+++ src/test/java/testNG/TestAnnotation.java
@@ -0,0 +1,22 @@
+package testNG;
+
+import org.testng.annotations.Test;
+
+public class TestAnnotation {
+    @Test
+    public void testMethod1() {
+        SomeClassToTest obj = new SomeClassToTest();
+        int result = obj.sumNumbers(1, 2);
+        System.out.println("Running Test -> testMethod1");
+    }
+
+    @Test
+    public void testMethod2() {
+        System.out.println("Running Test -> testMethod2");
+    }
+
+    @Test
+    public void testMethod3() {
+        System.out.println("Running Test -> testMethod3");
+    }
+}
