3f7d083d59efc43605dc661ba1a5af10d271f426
havuong <havuong138@gmail.com>
2018-10-08 18:26:11 +0700

    [TestNG] Soft Asserts

---
+++ src/test/java/testNG/SoftAsserts.java
@@ -0,0 +1,19 @@
+package testNG;
+
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class SoftAsserts {
+    @Test
+    public void testSum() {
+        SoftAssert sa = new SoftAssert();
+        System.out.println("\nRunning Test -> testSum");
+        SomeClassToTest obj = new SomeClassToTest();
+        int result = obj.sumNumbers(1, 2);
+        sa.assertEquals(result, 2);
+        System.out.println("\nLine after assert 1");
+        sa.assertEquals(result, 3);
+        System.out.println("\nLine after assert 2");
+        sa.assertAll();
+    }
+}
