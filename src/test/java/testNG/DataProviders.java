71dde1eefe90d12f909f6fbfc09f6985f34e9d06
havuong <havuong138@gmail.com>
2018-10-15 19:58:57 +0700

    [TestNG] DataProviders - Part 2

--- src/test/java/testNG/DataProviders.java
+++ src/test/java/testNG/DataProviders.java
@@ -4,16 +4,17 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class DataProviders {
-    @DataProvider(name="inputs")
-    public Object[][] getData() {
-        return new Object[][] {
-                {"bmw", "m3"},
-                {"audi", "a6"},
-                {"benz", "c300"}
-        };
-    }
 
-    @Test(dataProvider="inputs")
+//	@DataProvider(name="inputs")
+//	public Object[][] getData() {
+//		return new Object[][] {
+//			{"bmw", "m3"},
+//			{"audi", "a6"},
+//			{"benz", "c300"}
+//		};
+//	}
+
+    @Test(dataProvider="inputs", dataProviderClass=TestData.class)
     public void testMethod1(String input1, String input2) {
         System.out.println("Input 1: " + input1);
         System.out.println("Input 2: " + input2);
