c8684fa79c634baa67c98db989dac40861580bbc
havuong <havuong138@gmail.com>
2018-10-15 19:06:59 +0700

    [TestNG] DataProviders

---
+++ src/test/java/testNG/DataProviders.java
@@ -0,0 +1,21 @@
+package testNG;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class DataProviders {
+    @DataProvider(name="inputs")
+    public Object[][] getData() {
+        return new Object[][] {
+                {"bmw", "m3"},
+                {"audi", "a6"},
+                {"benz", "c300"}
+        };
+    }
+
+    @Test(dataProvider="inputs")
+    public void testMethod1(String input1, String input2) {
+        System.out.println("Input 1: " + input1);
+        System.out.println("Input 2: " + input2);
+    }
+}
