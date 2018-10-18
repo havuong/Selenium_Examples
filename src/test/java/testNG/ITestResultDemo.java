ac69881241ce2fe20749d2dc33baed5e129fcde1
havuong <havuong138@gmail.com>
2018-10-18 19:18:05 +0700

    [TestNG] Listeners - IInvokedMethodListener

--- src/test/java/testNG/ITestResultDemo.java
+++ src/test/java/testNG/ITestResultDemo.java
@@ -18,6 +18,10 @@ public class ITestResultDemo {
         Assert.assertTrue(true);
     }
 
+    //We can get the status of the @Test method in @AfterMethod using ITestResult.
+    //Once we have the status, then we can use that to decide what action we want to perform.
+    //The best example is to take screenshots.
+    //If the status of the test method is failed, then we can take a screenshot in the @AfterMethod.
     @AfterMethod
     public void afterMethod(ITestResult testResult) {
         if (testResult.getStatus() == ITestResult.FAILURE) {
