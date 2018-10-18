886ad8f2c496427b96a486c76f3a42836d24c1fb
havuong <havuong138@gmail.com>
2018-10-18 20:47:13 +0700

    [TestNG] Listeners - ISuiteListener

--- src/test/java/testNG/ListenersTest2.java
+++ src/test/java/testNG/ListenersTest2.java
@@ -7,27 +7,27 @@ import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import testNG.listenerspackage.CustomListener2;
 
-@Listeners(CustomListener2.class)
+//@Listeners(CustomListener2.class)
 public class ListenersTest2 {
     @BeforeClass
     public void setUp() {
-        System.out.println("Code in before class");
+        System.out.println("TestNG_ListenersTest2 -> Code in before class");
     }
 
     @AfterClass
     public void cleanUp() {
-        System.out.println("Code in after class");
+        System.out.println("TestNG_ListenersTest2 -> Code in after class");
     }
 
     @Test
     public void testMethod1() {
-        System.out.println("Code in testMethod1");
+        System.out.println("TestNG_ListenersTest2 -> Code in testMethod1");
         Assert.assertTrue(true);
     }
 
     @Test
     public void testMethod2() {
-        System.out.println("Code in testMethod2");
+        System.out.println("TestNG_ListenersTest2 -> Code in testMethod2");
         Assert.assertTrue(false);
     }
 }
